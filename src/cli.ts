1341938e582c25c2b9e6160785633b71ec7ebe14
Agusx1211 <agusgit@pm.me>
2025-08-03 13:22:58 +0000

    Make run the default command

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,13 @@ import { Command } from 'commander'
 import { makeRunCommand, makeDryRunCommand, makeListCommand, makeUtilsCommand } from './commands'
 
 export function setupCommands(program: Command): void {
-  program.addCommand(makeRunCommand())
+  // Make run the default command when no subcommand is provided
+  program.addCommand(makeRunCommand(), {
+    isDefault: true,
+    hidden: false // Keep it visible in help
+  })
+  
+  // Add other commands as subcommands
   program.addCommand(makeDryRunCommand())
   program.addCommand(makeListCommand())
   program.addCommand(makeUtilsCommand())
